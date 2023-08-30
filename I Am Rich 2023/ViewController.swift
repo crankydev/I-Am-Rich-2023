8fcce24297e87380340e8df589cbd3f9307570c2
Rachel Woodward <magicalsoprano@gmail.com>
2023-08-30 12:46:15 -0500

    Initial Commit

---
+++ I Am Rich 2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich 2023
+//
+//  Created by Rachel Woodward on 8/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
